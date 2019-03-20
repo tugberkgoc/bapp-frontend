91d9ce5c60b1e544e52477517b44222c90eaaa9f
tugberkgoc <tugberkgoc@gmail.com>
2019-03-20 22:31:47 +0300

    Add Zeit to the Project

---
+++ .vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'My VuePress Project',
+  description: 'A simple VuePress project deployed with ZEIT Now.',
+  themeConfig: {
+    nav: [{ text: 'Starter', link: '/dashboard' }, { text: 'UserProfile', link: '/user-profile' }]
+  }
+};
